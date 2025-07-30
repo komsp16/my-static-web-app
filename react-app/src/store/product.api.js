b1efb2f82cc9437e91f61d78fd71791a76d4fb37
komsp16 <kaumil.parekh@outlook.com>
2025-07-30 12:20:08 +1000

    Updated Product names to test ci/cd

--- react-app/src/store/product.api.js
+++ react-app/src/store/product.api.js
@@ -2,19 +2,19 @@ const data = {
   products: [
     {
       id: 10,
-      name: 'Strawberries',
-      description: '16oz package of fresh organic strawberries',
+      name: 'Strawberries for Kids',
+      description: '18oz package of fresh organic strawberries',
       quantity: '1',
     },
     {
       id: 20,
-      name: 'Sliced bread',
+      name: 'Sliced bread for family',
       description: 'Loaf of fresh sliced wheat bread',
       quantity: 1,
     },
     {
       id: 30,
-      name: 'Apples',
+      name: 'Apples for everyone',
       description: 'Bag of 7 fresh McIntosh apples',
       quantity: 1,
     },
